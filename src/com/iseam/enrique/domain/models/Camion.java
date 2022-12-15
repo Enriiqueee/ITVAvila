51fc42f235f8c352a22a5636cb37d66c5c62f09d
enriiquee <94898081+Enriiqueee@users.noreply.github.com>
2022-12-15 18:41:16 +0100

    primer push

---
+++ src/com/iseam/enrique/domain/models/Camion.java
@@ -0,0 +1,12 @@
+package com.iseam.enrique.domain.models;
+
+public class Camion {
+    private String tara;
+    public String getTara() {
+        return tara;
+    }
+
+    public void setTara(String tara) {
+        this.tara = tara;
+    }
+}
